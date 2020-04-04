877a17769b31952362e0f73edacbb0d9cdff7892
Andrea Tota <andrea.tota6@gmail.com>
2020-04-04 16:14:44 +0200

    Modified seeds and routes. Subsituted Faker datas with parsed data from API, added routes for doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root to: "cocktails#index"
-  resources :cocktails, only: [:index, :new, :show, :create]
+  root to: 'cocktails#index'
+  resources :cocktails, only: %i[index new show Screate] do
+    resources :doses, only: %i[new create]
+  end
+
+  resources :doses, only: [:delete]
 end
