b1772a5ea3d5f2f1540a60d112b40512909d127c
Andrea Tota <andrea.tota6@gmail.com>
2020-04-06 10:09:23 +0200

    Fix probelms related to cocktail_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/delete'
   root to: 'cocktails#index'
   resources :cocktails, only: %i[index new show create] do
     resources :doses, only: %i[new create]
   end
 
-  resources :doses, only: [:delete]
+  resources :doses, only: [:destroy]
 end
